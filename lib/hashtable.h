d17d385c82adb215e8c4a9e0d68a1c77fd554eed
seth <seth@cmu.edu>
2019-05-17 21:22:55 -0400

    use HashTable as uer visible type

--- lib/hashtable.h
+++ lib/hashtable.h
@@ -1,24 +1,24 @@
 #ifndef _HASHTABLE_H_
 #define _HASHTABLE_H_
 
-typedef struct TableHead TableHead;
+typedef struct TableHead HashTable;
 
 typedef struct entry{
   unsigned long val;
 }entry;
 
 // free hash table when done
-double freeAll(TableHead* head, int last);
+double freeAll(HashTable* head, int last);
 
 // see if entry is in the table
-int checkTableQuery(TableHead* head, entry* ent);
+int checkTableQuery(HashTable* head, entry* ent);
 
 // initialize a new main hashtable
-TableHead* initTable(TableHead* head, int InitSize, int HashAttempts);
+TableHead* initTable(HashTable* head, int InitSize, int HashAttempts);
 
 
 // return 1 if inserted, 0 if already there
-int insertTable(TableHead* head,  int start, entry* ent);
+int insertTable(HashTable* head,  int start, entry* ent);
 
 
 #endif
