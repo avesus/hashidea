d288377e3d95ee46d401d7309112ae8cf6accfc7
noah <goldstein.n@wustl.edu>
2019-06-21 12:11:55 -0500

    added -a arg to test script

--- src/test.sh
+++ src/test.sh
@@ -10,10 +10,13 @@ for table in hashtable_lazy_local hashtable_lazy hashtable_ll_rr hashtable_ll_tr
 	for qp in 0 0.5 0.9 0.99; do
 	    #echo "Running for qp $qp"
 	    for it in $(( inserts * 2 )) $inserts 8192; do
-		in=$(( inserts / t ))
-		#echo "running with initial table size $it and $in inserts"
-		echo ./harness --trials $trials --inserts $in --qp $qp -t $t -i $it
-		./harness --trials $trials --inserts $in --qp $qp -t $t -i $it
+		for ha in {1..5}; do
+		    in=$(( inserts / t ))
+		    #echo "running with initial table size $it and $in inserts"
+		    echo ./harness --trials $trials --inserts $in --qp $qp -t $t -i $it -a $ha
+		    ./harness --trials $trials --inserts $in --qp $qp -t $t -i $it -a $ha
+		    sleep 1
+		done
 	    done
 	done
     done
