aedfc93472cf56c945352b3cb229a51b187703c5
seth <seth@cmu.edu>
2019-07-01 10:12:00 -0400

    reduce number of options.

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-trials=30
+trials=20
 inserts=16000000
 
 make clean
@@ -12,10 +12,10 @@ for table in hashtable_lazy_local hashtable_lazy hashtable hashtable_ll_tr hasht
     else
 	for t in 1 2 4 8 16; do
 	    #echo "Running for threads $t"
-	    for qp in 0 0.5 0.9 0.99; do
+	    for qp in 0 0.5 0.9; do
 		#echo "Running for qp $qp"
-		for it in $(( inserts * 2 )) $inserts 8192; do
-		    for ha in {1..5}; do
+		for it in $(( inserts * 2 )) 8192; do
+		    for ha in {1..4}; do
 			in=$(( inserts / t ))
 			#echo "running with initial table size $it and $in inserts"
 			echo ./harness --trials $trials --regtemp --inserts $in --qp $qp -t $t -i $it -a $ha --regtemp --args
