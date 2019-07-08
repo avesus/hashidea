a0604c699d9953141507ba0a7c6abcad804fc86e
seth <seth@cmu.edu>
2019-07-08 16:45:24 -0400

    deal with errors (i.e., start but no end)

--- graphit/getstats.py
+++ graphit/getstats.py
@@ -2,8 +2,19 @@
 
 import argparse
 # import subprocess
-# from pprint import pprint
+from pprint import pprint
 import sys
+
+
+def resetState(args):
+    global state, elimprefix, outdata
+    state = 'START'
+    hashtype = args[0].split(':')[0].split("/")
+    hashtype.append(args[0].split(':')[1])
+    prefix = hashtype
+    prefix.extend(args[1:-1])
+    elimprefix = len(line.split(","))-1
+    outdata = prefix
 
 
 ################################################################
@@ -11,11 +22,13 @@ import sys
 parser = argparse.ArgumentParser(description='generate graphs from harness data')
 parser.add_argument("-v", "--verbosity", action="count", default=0, help="increase output verbosity")
 parser.add_argument("-o", "--out", default="", help="output file, defaults to stdout")
-parser.add_argument("datafile", nargs=1, help="file with data in it.")
+parser.add_argument("-w", "--window", type=int, default=0, help="prev window lines shown on error")
+parser.add_argument("datafile", nargs="+", help="file with data in it.")
 # parse arguments
 flags = parser.parse_args()
 verbose = flags.verbosity
 inname = flags.datafile[0]
+windowsize = flags.window
 outname = flags.out
 if outname == "":
     outfile = sys.stdout
@@ -26,39 +39,51 @@ state = None
 heading = []
 hasheading = {}
 result = []
-with open(inname, "r") as infile:
-    for line in infile:
-        line = line.strip()
-        args = [x.strip() for x in line.split(",")]
-        last = args[len(args)-1]
-        if state is None:
-            if last == 'START':
-                state = last
-                hashtype = args[0].split(':')[0].split("/")
-                hashtype.append(args[0].split(':')[1])
-                prefix = hashtype
-                prefix.extend(args[1:-1])
-                elimprefix = len(line.split(","))-1
-                outdata = prefix
-            elif len(heading) == 0 and last == 'HEADING':
-                heading = args[1:-1]
-                hprefix = ['main', 'probe', 'move', 'version']
-                hprefix.extend(heading)
-                heading = hprefix
-        elif state == 'START':
-            args = args[elimprefix:]
-            if last == 'END':
-                state = None
-                # print("{}\n".format(",".join(outdata)))
-                result.append(outdata)
-                outdata = []
-            kind = args[0]
-            args = args[1:]
-            if kind not in hasheading:
-                hasheading[kind] = 1
-                tags = ["-".join([kind, x.split(':')[0]]) for x in args]
-                heading.extend(tags)
-            outdata.extend([x.split(':')[1] for x in args])
+for inname in flags.datafile:
+    sys.stderr.write("{}\n".format(inname))
+    window = []
+    with open(inname, "r") as infile:
+        ln = 0
+        sys.stderr.write("{}:{}\r".format(inname, ln))
+        for line in infile:
+            ln += 1
+            line = line.strip()
+            window.append(line)
+            args = [x.strip() for x in line.split(",")]
+            last = args[len(args)-1]
+            # pprint(args)
+            if state is None:
+                if last == 'START':
+                    resetState(args)
+                elif len(heading) == 0 and last == 'HEADING':
+                    heading = args[1:-1]
+                    hprefix = ['main', 'probe', 'move', 'version']
+                    hprefix.extend(heading)
+                    heading = hprefix
+            elif state == 'START':
+                if verbose:
+                    print('{}:{}:{}'.format(ln, last, line))
+                oldargs = args
+                args = args[elimprefix:]
+                if last == 'END':
+                    state = None
+                    # print("{}\n".format(",".join(outdata)))
+                    result.append(outdata)
+                    outdata = []
+                elif last == 'START':
+                    sys.stderr.write('Missing end, 2 STARTS? {}:{}\n'.format(inname, ln))
+                    if windowsize > 0:
+                        sys.stderr.write('{}\n'.format("\n".join(window[-windowsize:])))
+                    resetState(oldargs)
+                elif len(args) > 0:
+                    kind = args[0]
+                    args = args[1:]
+                    if kind not in hasheading:
+                        hasheading[kind] = 1
+                        tags = ["-".join([kind, x.split(':')[0]]) for x in args]
+                        # print('Extending hashheading:{} {} {}'.format(ln, kind, ",".join(tags)))
+                        heading.extend(tags)
+                    outdata.extend([x.split(':')[1] for x in args])
 
 outfile.write("{}\n".format(",".join(heading)))
 for line in result:
